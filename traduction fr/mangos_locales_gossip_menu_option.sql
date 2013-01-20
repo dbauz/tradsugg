4dbc5096684df4c3e3dee5cfd5f67cd69a089fa4
dbauz <dbauz@free.fr>
2013-01-20 08:55:51 +0100

    ajout trad gossip 7892 AND `id` = 1
    Signed-off-by: dbauz <dbauz@free.fr>

--- traduction fr/mangos_locales_gossip_menu_option.sql
+++ traduction fr/mangos_locales_gossip_menu_option.sql
@@ -789,5 +789,6 @@ UPDATE `locales_gossip_menu_option` SET `option_text_loc2` = 'Votre conjoint?' W
 UPDATE `locales_gossip_menu_option` SET `option_text_loc2` = 'Je voudrais me former davantage dans les voies de la lumière' WHERE `menu_id` = 6647  AND `id` =1 ;
 UPDATE `locales_gossip_menu_option` SET `option_text_loc2` = 'En savoir plus sur Double spécialisation des talents' WHERE `menu_id` = 6647 AND `id` = 2;
 UPDATE `locales_gossip_menu_option` SET `option_text_loc2` = 'Je tiens à désapprendre mes talents.' WHERE `menu_id` = 6647 AND `id` = 3;
+UPDATE `locales_gossip_menu_option` SET `option_text_loc2` = 'J\'ai entendu dire que votre chien Fei Fei a pris le chapelet de Klatu...' WHERE `menu_id` = 7892 AND `id` = 1 ;
 
 
