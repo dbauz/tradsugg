02d485f852e0cfd40db028b563c79f292ae5b1aa
dbauz <dbauz@free.fr>
2013-03-03 22:25:36 +0100

    ajout de suggestion sd2 mangos 2802
    Signed-off-by: dbauz <dbauz@free.fr>

---
+++ suggestion Xfury/suggestion sd2 mangos 2802.sql
@@ -0,0 +1,4 @@
+DELETE FROM creature_linking_template WHERE entry IN (29713,29680);
+INSERT INTO creature_linking_template (entry, map, master_entry, flag, search_range) VALUES
+(29713, 604, 29304, 4112, 0),
+(29680, 604, 29304, 4112, 0);
