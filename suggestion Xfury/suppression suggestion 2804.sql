d55a6c06e0969b5763735f90a81f01c9ec72413d
dbauz <dbauz@free.fr>
2013-03-05 23:49:46 +0100

    obsolète, supprimé automatiquement par commit 2808
    Signed-off-by: dbauz <dbauz@free.fr>

--- suggestion Xfury/suppression suggestion 2804.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM creature_linking_template WHERE entry IN (28166);
-DELETE FROM db_script_string WHERE entry BETWEEN 2000005511 AND 2000005514;
-DELETE FROM dbscripts_on_creature_death WHERE id=27654;
-DELETE FROM dbscripts_on_creature_death WHERE id=27447;
-DELETE FROM dbscripts_on_creature_death WHERE id=27655;
