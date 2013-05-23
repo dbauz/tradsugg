f04d18e682755e62014aafb76980ee5ec97143d1
dbauz <dbauz@free.fr>
2013-05-23 16:22:48 +0200

    ajout de suggestion sd2 mangos 2899
    Signed-off-by: dbauz <dbauz@free.fr>

---
+++ suggestion Xfury/suggestion sd2 mangos 2899.sql
@@ -0,0 +1,9 @@
+delete from spell_script_target where entry = 45388;
+insert into spell_script_target values
+(45388, 1, 25038, 0);
+delete from spell_script_target where entry = 45389;
+insert into spell_script_target values
+(45389, 1, 25265, 0);
+delete from spell_script_target where entry = 45714;
+insert into spell_script_target values
+(45714, 1, 25038, 0);
