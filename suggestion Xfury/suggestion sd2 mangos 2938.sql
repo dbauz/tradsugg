5b0c8c79b329159ee67ca84171f7a551eae5b244
dbauz <dbauz@free.fr>
2013-09-23 09:51:38 +0200

    ajout de suggestion sd2 mangos 2938 et 2939
    Signed-off-by: dbauz <dbauz@free.fr>

---
+++ suggestion Xfury/suggestion sd2 mangos 2938.sql
@@ -0,0 +1,4 @@
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN (10074,10075) AND type IN (11,18);
+INSERT INTO achievement_criteria_requirement VALUES
+(10074,18,0,0),
+(10075,18,0,0);
