72d5317bf81c3e63ec031be3c7c755f41df6ae8a
dbauz <dbauz@free.fr>
2013-11-25 08:06:07 +0100

    correction erreur de syntaxe
    Signed-off-by: dbauz <dbauz@free.fr>

--- Script Acid/Lord Gregor Lescovar and Marzon the Silent Blade.sql
+++ Script Acid/Lord Gregor Lescovar and Marzon the Silent Blade.sql
@@ -6,7 +6,7 @@ UPDATE creature_template SET AIName='EventAI' WHERE `entry` = '1754';
 DELETE FROM `creature_ai_texts` WHERE `entry` =-9000;
 INSERT into `creature_ai_texts` (`entry`,`content_default`,`sound`,`type`,`language`,`comment`,`emote`) VALUES
 ('-9000','What? How dare you!','0','0','0','1754','0');
-UPDATE `creature_ai_texts` SET `content_loc2`='Quoi? Comment osez-vous!' WHERE (`entry`='-9000')
+UPDATE `creature_ai_texts` SET `content_loc2`='Quoi? Comment osez-vous!' WHERE (`entry`='-9000');
 -- Marzon the Silent Blade
 Delete from `creature_ai_scripts` where `id`=175501;
 Insert into `creature_ai_scripts` values 
