6a434df4c0d8108ea4b831f6af127219e0116579
dbauz <dbauz@free.fr>
2013-12-13 12:53:15 +0100

    MAJ ACID 13-12-2013
    Signed-off-by: dbauz <dbauz@free.fr>

---
+++ Script Acid/MAJ ACID 13-12-2013.sql
@@ -0,0 +1,5 @@
+DELETE FROM creature_ai_scripts WHERE creature_id=17264;
+INSERT INTO creature_ai_scripts VALUES 
+('1726401','17264','4','0','20','6','0','0','0','0','1','-156','-181','-672','0','0','0','0','0','0','0','0','Bonechewer Ravener - Random Say on Aggro'),
+('1726402','17264','0','0','100','7','3200','8800','20100','30100','11','30621','1','0','13','-40','1','0','0','0','0','0','Bonechewer Ravener - Cast Kidney Shot and Reduce Aggro');
+DELETE FROM creature_ai_texts where entry=-673;
